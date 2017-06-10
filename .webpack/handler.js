0842699202e9917b4dc7b35302370c5ca0e5b7e8
Derek Alia <derekalia@me.com>
2017-06-09 18:32:51 -0700

    first commit

---
+++ .webpack/handler.js
@@ -0,0 +1,80 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _stringify = __webpack_require__(7);
+
+	var _stringify2 = _interopRequireDefault(_stringify);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	module.exports.hello = function (event, context, callback) {
+	  var response = {
+	    statusCode: 200,
+	    body: (0, _stringify2.default)({
+	      message: 'Go Serverless v1.0! Your function executed successfully!',
+	      input: event
+	    })
+	  };
+
+	  callback(null, response);
+
+	  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+	  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+	};
+
+/***/ }),
+
+/***/ 7:
+/***/ (function(module, exports) {
+
+	module.exports = require("babel-runtime/core-js/json/stringify");
+
+/***/ })
+
+/******/ })));
